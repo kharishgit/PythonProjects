5eacee5f516bff3ccd45594a08c37946c420f980
harish.k3 <harish.k@pixdynamics.com>
2025-05-26 16:53:32 +0530

    Arrays

--- Samples/Ex.py
+++ Samples/Ex.py
@@ -1,12 +1,3 @@
-def dailyTemperatures(temperatures):
-    res = [0] * len(temperatures)
-    stack = []
-    for i,t in enumerate(temperatures):
-        while stack and t > stack[-1][0]:
-            stackT, stackInd = stack.pop()
-            print(stackT , stackInd)
-            res[stackInd] = (i-stackInd)
-        stack.append([t,i])
-    return res
-
-print(dailyTemperatures([73,74,75,71,69,72,76,73]))
+nums = [1,2,3]
+squares = list(map(lambda x : x*2,nums))
+print(squares)
