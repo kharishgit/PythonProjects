9014769ac113aca21fd51d0d705b2bfee8b9ee14
harish.k3 <harishgetsmail@gmail.com>
2026-02-06 11:12:30 +0530

    On Feb 2026

--- IPSR/Numpy/5_array_properties.py
+++ IPSR/Numpy/5_array_properties.py
@@ -1,6 +1,8 @@
 import numpy as np
-arr = np.array([[1, 2, 3], [4, 5, 6]],dtype=np.int32)
+arr = np.array([[1, 2, 3], [4, 5, 6]],dtype=np.int64)
 print("Shape:", arr.shape)
 print("Size:", arr.size)
 print("Dimension:", arr.ndim)
 print("Datatype:", arr.dtype)
+
+
